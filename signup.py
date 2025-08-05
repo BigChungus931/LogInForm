a906359ef8439d22bcde18c6c1ea0f96a5e4d571
BigChungus931 <borsoshkin@gmail.com>
2025-08-05 11:15:18 -0600

    working on a sign up page

--- signup.py
+++ signup.py
@@ -13,12 +13,25 @@ frame.place(relx=0.5, rely=0.5, anchor="center")
 title_Label = tk.Label(frame, text="Sign up", font=("Arial", 14, "bold"), bg="white")
 title_Label.pack(pady=(0,20))
 
-tk.Label(frame, text="Create username", bg="white").pack()
-name_entry = tk.Entry(frame, width=30)
-name_entry.pack(pady=5)
+tk.Label(frame, text="Display first name", bg="white").pack()
+firstname_entry = tk.Entry(frame, width=30)
+firstname_entry.pack(pady=5)
 
-tk.Label(frame, text="Create Password", bg="white").pack()
+tk.Label(frame, text="Display last name", bg="white").pack()
+lastname_entry = tk.Entry(frame, width=30)
+lastname_entry.pack(pady=5)
+
+tk.Label(frame, text="Create username", bg="white").pack()
+username_entry = tk.Entry(frame, width=30)
+username_entry.pack(pady=5)
+
+tk.Label(frame, text="Create password", bg="white").pack()
 password_entry = tk.Entry(frame, width=30, show="*")
 password_entry.pack(pady=5)
+
+tk.Label(frame, text="Confirm password", bg="white").pack()
+password_verify_entry = tk.Entry(frame, width=30)
+password_verify_entry.pack(pady=5)
+
 
 window.mainloop()
