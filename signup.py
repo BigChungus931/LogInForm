eb8b6da2ed45dbd53a0e018c345d4f7bca4b3d01
BigChungus931 <borsoshkin@gmail.com>
2025-09-28 09:51:58 -0600

    Continued designing the signup page and added an option where you can go to the login page

--- signup.py
+++ signup.py
@@ -3,12 +3,18 @@ from tkinter import messagebox
 import mysql.connector
 from mysql.connector import Error
 import bcrypt
+from PIL import Image, ImageTk
 
 window = tk.Tk()
 window.title("Sign up")
-window.geometry("700x600")
+window.geometry("700x700")
 window.resizable(False, False)
 window.config(bg="white")
+
+logo = Image.open("img/Computer-logo.png")
+resized_logo = logo.resize((300, 90))
+photo = ImageTk.PhotoImage(resized_logo)
+y_step = 70
 
 try:
     connection = mysql.connector.connect(
@@ -91,44 +97,62 @@ try:
                 messagebox.showinfo("Success", "User registered successfully")
 
 
-        frame = tk.Frame(window, bg="white", padx=20, pady=10, width=600, height=700, relief="groove")
-        frame.pack(pady=100)
+        frame = tk.Frame(window, bg="white", padx=20, pady=10, width=600, height=900, relief="groove")
+        frame.pack(pady=70)
         frame.pack_propagate(False)
 
-        title_Label = tk.Label(frame, text="Sign up", font=("Arial", 14, "bold"), bg="white")
-        title_Label.pack(pady=(0,20))
+        logo_label = tk.Label(frame, image=photo, bd=0, highlightthickness=0)
+        logo_label.pack(pady=0)
 
-        tk.Label(frame, text="Display first name", bg="white").place(x=0, y=70)
+        # title_Label = tk.Label(frame, text="Sign up", font=("Arial", 14, "bold"), bg="white")
+        # title_Label.pack(pady=(0,20))
+
+        tk.Label(frame, text="Display first name", bg="white").place(x=0, y=70 + y_step)
         firstname_entry = tk.Entry(frame, width=45, relief="ridge")
-        firstname_entry.place(x=0, y=100)
+        firstname_entry.place(x=0, y=100 + y_step)
 
-        tk.Label(frame, text="Display last name", bg="white").place(x=300, y=70)
+        tk.Label(frame, text="Display last name", bg="white").place(x=300, y=70 + y_step)
         lastname_entry = tk.Entry(frame, width=45, relief="ridge")
-        lastname_entry.place(x=300, y=100)
+        lastname_entry.place(x=300, y=100 + y_step)
 
-        tk.Label(frame, text="Enter email address", bg="white").place(x=0, y=150)
+        tk.Label(frame, text="Enter email address", bg="white").place(x=0, y=150 + y_step)
         email_entry = tk.Entry(frame, width=45, relief="ridge")
-        email_entry.place(x=0, y=180)
+        email_entry.place(x=0, y=180 + y_step)
 
-        tk.Label(frame, text="Add your age", bg="white").place(x=300, y=150)
+        tk.Label(frame, text="Add your age", bg="white").place(x=300, y=150 + y_step)
         age_entry = tk.Entry(frame, width=45, relief="ridge")
-        age_entry.place(x=300, y=180)
+        age_entry.place(x=300, y=180 + y_step)
 
-        tk.Label(frame, text="Create password", bg="white").place(x=0, y=230)
+        tk.Label(frame, text="Create password", bg="white").place(x=0, y=230 + y_step)
         password_entry = tk.Entry(frame, width=45, show="*", relief="ridge")
-        password_entry.place(x=0, y=260)
+        password_entry.place(x=0, y=260 + y_step)
 
-        tk.Label(frame, text="Confirm password", bg="white").place(x=300, y=230)
+        tk.Label(frame, text="Confirm password", bg="white").place(x=300, y=230 + y_step)
         password_verify_entry = tk.Entry(frame, width=45, show="*", relief="ridge")
-        password_verify_entry.place(x=300, y=260)
+        password_verify_entry.place(x=300, y=260 + y_step)
 
-        tk.Label(frame, text="Enter your country", bg="white").place(x=225, y=310)
+        tk.Label(frame, text="Enter your country", bg="white").place(x=225, y=310 + y_step)
         country_entry = tk.Entry(frame, width=95, relief="ridge")
-        country_entry.place(x=0, y=340)
+        country_entry.place(x=0, y=340 + y_step)
 
 
-        signin_button = tk.Button(frame, text="Submit", bg="#4CAF50", fg="white", width="25", command=add_user, relief="flat")
-        signin_button.place(y=370, x=190)
+        signin_button = tk.Button(frame, text="Signup", bg="#4CAF50", fg="white", width="25", command=add_user, relief="flat", cursor="hand2")
+        signin_button.place(y=395 + y_step, x=190)
+
+        exists = tk.Label(frame, text="Already have an account?", bg="white")
+        exists.place(y=450 + y_step, x=190)
+
+        login = tk.Button(frame, text="Login", fg="#4CAF50", bg="white", relief="flat", cursor="hand2")
+        login.place(y=448 + y_step, x=330)
+
+        def on_enter(event):
+            login.config(fg="red")
+        def on_leave(event):
+            login.config(fg="#4CAF50")
+
+        login.bind("<Enter>", on_enter)
+        login.bind("<Leave>", on_leave)
+
 
 except Error as e:
     print(f"Error connecting to database {e}")
